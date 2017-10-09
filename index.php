0eb374a88755853c0e052bd48f91a21f0acee8d9
Jerry L. Thao <thaoj09@gmail.com>
2017-10-09 08:36:46 -0700

    Login - Functional Buttons
    stuff

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$result_array = array();
+$conn = new mysqli('http://athena.ecs.csus.edu/~cs174131/','cs174131','oojbbidr','cs174131');
+if ($conn->connect_error) {
+
+     die("Connection to database failed: " . $conn->connect_error);
+}
+$sql = "SELECT *, FROM STUDENT";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+
+        array_push($result_array, $row);
+
+    }
+
+}
+
+$conn->close();
+?>
