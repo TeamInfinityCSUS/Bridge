b858c531e6c920a8abd916a80791d3d57e73bc66
thaoj0 <thaoj09@gmail.com>
2017-10-20 20:35:08 -0700

    Delete test.php

--- php/test.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-   if( $_REQUEST["name"] || $_REQUEST["age"] ) {
-      echo "Welcome ". $_REQUEST['name']. "<br />";
-      echo "You are ". $_REQUEST['age']. " years old.";
-      exit();
-   }
-?>
