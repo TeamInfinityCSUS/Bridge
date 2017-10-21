f9aec74db1a197224f1ca87303febdce4c69e063
thaoj0 <thaoj09@gmail.com>
2017-10-20 19:21:28 -0700

    Update test.php

--- php/test.php
+++ php/test.php
@@ -1,3 +1,8 @@
-(<?php 
-echo json_encode(array("name"=>"John","time"=>"2pm")); 
-?>)
+
+<?php
+   if( $_REQUEST["name"] || $_REQUEST["age"] ) {
+      echo "Welcome ". $_REQUEST['name']. "<br />";
+      echo "You are ". $_REQUEST['age']. " years old.";
+      exit();
+   }
+?>
