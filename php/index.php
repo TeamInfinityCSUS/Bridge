83c31a2692cc784ab19ef499743ba2111de6177e
thaoj0 <thaoj09@gmail.com>
2017-10-20 18:16:34 -0700

    Create index.php

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<?php
+$result_array = array(); // Hash are called Associative Array in PHP
+$input = $_POST['input']; // input is an array [user, password]
+$output = 0;
+// Data hashkey => [stringarray0, stringarray1] 
+$result_array[1]=['john@email.com','password'];
+
+foreach($result_array as $key => $value){
+  if ($input[0]==$value[0] && $input[1]==$value[1]){
+    $output=$key;
+    break;
+  }
+}
+
+echo $output; // if the output is 0 log in failed
+?>
