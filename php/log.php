91e2506839ba0664fe8f410e891424e4304b6c69
thaoj0 <thaoj09@gmail.com>
2017-10-24 10:50:25 -0700

    Create log.php

---
+++ php/log.php
@@ -0,0 +1,22 @@
+<?php
+$outgoing = 0;
+$incoming1 = $_POST['log'][0];
+$incoming2 = $_POST['log'][1];
+$conn = new mysqli('athena.ecs.csus.edu','bridge_user','bridge_db','bridge');
+if ($conn->connect_error) {
+     die("Connection to database failed: " . $conn->connect_error);
+}
+$sql = "SELECT email,pass,username FROM users;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) { // {Column_name_key : field_value}
+        if ((strtolower($incoming1)===$row['email'] || $incoming1===$row['username']) && $incoming2===$row['pass']){
+                $outgoing = $row['username'];
+        }
+    }
+}
+
+echo json_encode($outgoing);
+$conn->close();
+?>
