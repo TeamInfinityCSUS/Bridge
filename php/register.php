36f0fc07527c0933b1ca83e59dc24bd20ee6cb2f
Eljin013 <eligio013@gmail.com>
2017-10-25 14:40:39 -0700

    Updated Register

---
+++ php/register.php
@@ -0,0 +1,9 @@
+<?php
+$conn = new mysqli('athena.ecs.csus.edu','bridge_user','bridge_db','bridge'); // Opens Database
+if ($conn->connect_error) { // Connection Check
+     die("Connection to database failed: " . $conn->connect_error);
+}
+
+
+$conn->close(); // Close Connection
+?>
