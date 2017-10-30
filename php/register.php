148d41c4732ccfa1a0735b8837a35bcd2189d4e9
Eljin013 <eligio013@gmail.com>
2017-10-30 13:37:37 -0700

    updated Register

--- php/register.php
+++ php/register.php
@@ -4,6 +4,15 @@ if ($conn->connect_error) { // Connection Check
      die("Connection to database failed: " . $conn->connect_error);
 }
 
+$addStudent = "INSERT INTO site_member(username, firstname, lastname, password, email)
+VALUES ($student[0], $student[1], $student[2], $student[3], $student[4])";
+
+if($conn->query($addStudent) === TRUE) {
+
+  echo "console.log('New record created successfully')";
+} else {
+  echo "console.log('Error: ' . $addStudent . '<br>' . $conn->error)";
+}
 
 $conn->close(); // Close Connection
 ?>
